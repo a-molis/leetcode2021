fa61cddf95931328992d31a5383ecec3b9a7fe9e
Andrew Molis <andrewmolis@gmail.com>
2021-02-08 15:33:27 -0800

    Added containsDuplicate

---
+++ src/main/java/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] testArray = {1, 1, 2, 3, 4, 5};
+        System.out.println(containsDuplicate(testArray));
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        HashSet<Integer> singleNums = new HashSet<>();
+        for (Integer num: nums) {
+          if (singleNums.contains(num)) {
+              return true;
+          } else {
+              singleNums.add(num);
+          }
+        }
+        return false;
+    }
+}
